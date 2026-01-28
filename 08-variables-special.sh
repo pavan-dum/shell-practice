dee8374215205756480450fbcf2e29195e958502
pavan-dum <pavankumardumpala3941@gmail.com>
2026-01-28 19:20:27 +0530

    vars with specil type2

--- 08-variables-special.sh
+++ 08-variables-special.sh
@@ -7,11 +7,11 @@ echo "number of vars passed:        $#"
 
 echo "script name:                  $0"
 
-echo "present which directory you have: $pwd"
+echo "present which directory you have: $PWD"
 
-echo "who is running this script:      $user"
+echo "who is running this script:      $USER"
 
-echo "home directory of the user:      $home"
+echo "home directory of the user:      $HOME"
 
 echo "PID of the script:               $$"
 
