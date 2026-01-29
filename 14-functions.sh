291e05347d625085e4a273a72578245c99f8d414
pavan-dum <pavankumardumpala3941@gmail.com>
2026-01-29 19:55:30 +0530

    function2

--- 14-functions.sh
+++ 14-functions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-func ( $@ ) {
+func () {
     dnf install $2 -y
-    if [ $1 -ne 0]; then
+    if [ $1 -ne 0 ]; then
         echo "$2 Installation.....FAILURE"
     else
         echo "$2 Installation.....SUCCESS"
