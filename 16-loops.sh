33828b10118ea3760b354ff72978bda4f41ca8f1
pavan-dum <pavankumardumpala3941@gmail.com>
2026-01-31 21:46:09 +0530

    loops2

--- 16-loops.sh
+++ 16-loops.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-for i in {0..100}
+# for i in {0..100}
+# do
+#     echo "num:$i"
+# done
+
+
+
+validate (){
+    if [$1 -ne 0 ]; then
+        echo "$2....FAILURE"
+    else
+        echo "$2....SUCCESS"
+    fi
+}
+for package in "$@"
 do
-    echo "num:$i"
+   dnf install $package -y
+   validate $? "$pacakage Installation"
+
 done
