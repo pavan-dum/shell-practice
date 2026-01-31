1f19dc116f42ff0d01493cc2b24888b9b555cacd
pavan-dum <pavankumardumpala3941@gmail.com>
2026-01-31 21:48:13 +0530

    loops3

--- 16-loops.sh
+++ 16-loops.sh
@@ -5,18 +5,27 @@
 #     echo "num:$i"
 # done
 
+UUID=$(id -u)
+LOGS_FOLDER="/var/log/sample-logs"
+LOGS_FILE=$LOGS_FOLDER/$0.log
 
+if [ $UUID -ne 0 ]; then
+    echo "Please get ROOT ACCESS to execute."
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
 
 validate (){
     if [$1 -ne 0 ]; then
-        echo "$2....FAILURE"
+        echo "$2....FAILURE" | tee -a $LOGS_FILE
     else
-        echo "$2....SUCCESS"
+        echo "$2....SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 for package in "$@"
 do
-   dnf install $package -y
+   dnf install $package -y &>> $LOGS_FILE
    validate $? "$pacakage Installation"
 
 done
